e07d92bb125cb00e11f5cea499018076f4305b69
xiaokang <xiaokang94@sina.cn>
2019-02-13 20:24:44 +0800

    邮件类编写

--- admin/index/controller/Index.php
+++ admin/index/controller/Index.php
@@ -2,10 +2,25 @@
 
 namespace app\index\controller;
 
+use Tiny\Notify\MailMessage;
+use Tiny\Notify\MailNotify\MailNotify;
+use Tiny\Notify\MailObject;
+
 class Index
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+        require_once "E:/web/tiny/vendor/notify/autoload.php";
+        $username = "系统发送";
+        $sender = new MailObject($username, '41478184@qq.com', "vejohtiqnmpecada");
+        $receiver = new MailObject($username, "1259039066@qq.com");
+
+        $Message = new MailMessage();
+        $Message->subject = '这是一个测试的邮件';
+        $Message->body = "这是一个标题";
+
+
+        $mailNotify = new MailNotify();
+        $mailNotify->sendNotify($sender, $receiver, $Message);
     }
 }
